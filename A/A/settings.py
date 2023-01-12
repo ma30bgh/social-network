cf3ed11327bb9e021a51eaa5bc4b50197d6ab443
Masumeh <masomebaghshahi@gmail.com>
2023-01-13 00:46:34 +0330

    user information

--- A/A/settings.py
+++ A/A/settings.py
@@ -127,9 +127,5 @@ LOGIN_URL = '/account/login/'
 #Google account
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'masomebaghshahi@gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'nparnjynpeffqbov'
 EMAIL_USE_TLS = True
-#emza emaili k ersal mishe
-DEFAULT_FROM_EMAIL = 'ma30bgh'
